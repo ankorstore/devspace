d4ab66c7765f99b12bb1024700250c7c86ef1a4c
Jordi Clariana <jordi.clariana@ankorstore.com>
2023-04-19 10:47:48 +0200

    Update git repository address to point to ankorstore fork
    Signed-off-by: Jordi Clariana <jordi.clariana@ankorstore.com>

--- pkg/devspace/dependency/util/util_test.go
+++ pkg/devspace/dependency/util/util_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestSwitchURLType(t *testing.T) {
-	httpURL := "https://github.com/devspace-sh/devspace.git"
-	sshURL := "git@github.com:devspace-sh/devspace.git"
+	httpURL := "https://github.com/ankorstore/devspace.git"
+	sshURL := "git@github.com:ankorstore/devspace.git"
 
 	assert.Equal(t, sshURL, switchURLType(httpURL))
 	assert.Equal(t, httpURL, switchURLType(sshURL))
